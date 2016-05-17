fbe228bb4a049b05795cfb280d5b019f4a322597
Alejandro Baez <code@baez.nyc>
2016-05-17 18:28:37 -0400

    initial commit

---
+++ completions/textadept.fish
@@ -0,0 +1,13 @@
+complete -c textadept -s e -l execute -d "Run Lua code." -x
+
+complete -c textadept -s f -l force -d "Forces unique instance." -x
+
+complete -c textadept -s h -l help -d "Shows this." -x
+
+complete -c textadept -s n -l nosession -d "No session functionality." -x
+
+complete -c textadept -s u -l userhome -d "Sets alternate _USERHOME." -x
+
+complete -c textadept -s v -l version -d "Prints Textadept version and copyright" -x
+
+complete -c 'textadept-curses' -w textadept
